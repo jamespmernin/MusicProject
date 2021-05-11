cf1c55ae5132ccfe58079ac576aa9a4e26242f4a
James Mernin <jamespmernin@gmail.com>
2021-05-11 15:54:32 -0400

    adding album info

--- src/components/Albums.jsx
+++ src/components/Albums.jsx
@@ -6,13 +6,13 @@ function Albums(props) {
     <div>
       {/* <input type="text" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)}></input> */}
       <div className="map">
-        {Object.values(props).map((album, i) => /* Checking data.imgURL not equal to test avoids an error with displaying a bad album */
+        {Object.values(props)[1].slice(1).map((album, i) => /* Checking data.imgURL not equal to test avoids an error with displaying a bad album */
           <Link key={i} to={`/${i}`} >
             {/* eslint-disable-next-line*/}
             <div className="album" key={i} className="mappedAlbums">
-              <img key={i} src={album.imgURL} alt={album.name} />
-              <h2 key={album._id}>{album.name}</h2>
-              <h3 key={album.price}>${album.price}</h3>
+              <img key={i} src={album.artworkUrl100} alt={album.collectionName} />
+              <h2 key={album.collectionId}>{album.collectionName}</h2>
+              <h3 key={album.releaseDate}>{album.releaseDate}</h3>
             </div>
           </Link>
         )
