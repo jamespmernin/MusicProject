859b9f17ea27bf3ea173691a99a6ae0313f77a5d
James Mernin <jamespmernin@gmail.com>
2021-05-11 16:34:19 -0400

    closer to realizing iteration

--- src/components/Albums.jsx
+++ src/components/Albums.jsx
@@ -6,13 +6,13 @@ function Albums(props) {
     <div>
       {/* <input type="text" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)}></input> */}
       <div className="map">
-        {Object.keys(props).forEach((key) => /* Checking data.imgURL not equal to test avoids an error with displaying a bad album */
+        {Object.keys(props.albums).forEach((key) => /* Checking data.imgURL not equal to test avoids an error with displaying a bad album */
           <Link key={props[key]} to={`/${props[key]}`} >
             {/* eslint-disable-next-line*/}
             <div className="album" key={key} className="mappedAlbums">
               <img key={key} src={key.artworkUrl100} alt={key.collectionName} />
-              <h2 key={props[key].collectionId}>{props[key].collectionName}</h2>
-              <h3 key={props[key].releaseDate}>{props[key].releaseDate}</h3>
+              <h2 key={props.albums[key].collectionId}>{props.albums[key].collectionName}H2</h2>
+              <h3 key={props.albums[key].releaseDate}>{props.albums[key].releaseDate}H3</h3>
             </div>
           </Link>
         )
