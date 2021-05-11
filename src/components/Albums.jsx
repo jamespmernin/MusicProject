6792afa01d3f90ad9a098ad4f080c73e70bed204
James Mernin <jamespmernin@gmail.com>
2021-05-11 16:30:01 -0400

    figured out errors

--- src/components/Albums.jsx
+++ src/components/Albums.jsx
@@ -6,13 +6,13 @@ function Albums(props) {
     <div>
       {/* <input type="text" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)}></input> */}
       <div className="map">
-        {Object.values(props).map((type) => /* Checking data.imgURL not equal to test avoids an error with displaying a bad album */
-          <Link key={type} to={`/${type}`} >
+        {Object.keys(props).forEach((key) => /* Checking data.imgURL not equal to test avoids an error with displaying a bad album */
+          <Link key={props[key]} to={`/${props[key]}`} >
             {/* eslint-disable-next-line*/}
-            <div className="album" key={type} className="mappedAlbums">
-              {/* <img key={type} src={album.artworkUrl100} alt={album.collectionName} />
-              <h2 key={album.collectionId}>{album.collectionName}</h2>
-              <h3 key={album.releaseDate}>{album.releaseDate}</h3> */}
+            <div className="album" key={key} className="mappedAlbums">
+              <img key={key} src={key.artworkUrl100} alt={key.collectionName} />
+              <h2 key={props[key].collectionId}>{props[key].collectionName}</h2>
+              <h3 key={props[key].releaseDate}>{props[key].releaseDate}</h3>
             </div>
           </Link>
         )
