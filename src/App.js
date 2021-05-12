8e7c809d81579c72e9dff91a49099fe26491c26d
James Mernin <jamespmernin@gmail.com>
2021-05-12 13:10:41 -0400

    cleaning formatting and adding comments

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React, { useEffect, useState } from 'react'
 import { Route } from 'react-router-dom'
 import axios from 'axios';
 import Albums from './components/Albums'
-// import AlbumsDetail from './components/AlbumsDetail'
 import './App.css'
 
 function App() {
-  const [albums, setAlbums] = useState([]); // Easier to change albums using a state
+  const [albums, setAlbums] = useState([]); // Easier to change album information with a state
   useEffect(() => {
     const getAlbums = async () => {
-      const apiUrl = 'https://cors-anywhere.herokuapp.com/https://itunes.apple.com/lookup?id=136975&entity=album';
+      const apiUrl = 'https://cors-anywhere.herokuapp.com/https://itunes.apple.com/lookup?id=136975&entity=album'; // The CORS workaround avoids an issue with accessing the API
       const res = await axios(apiUrl);
       setAlbums(res.data);
     }
--- src/components/Albums.jsx
+++ src/components/Albums.jsx
@@ -1,24 +1,24 @@
+// This is what produces each album as its own component on the page.
 import React, { /*useState*/ } from 'react';
 
 function Albums(props) {
-  if (props.albums.results) {
-  console.log(props.albums.results.slice(1));
+  if (props.albums.results) { // this condition is required to access the data from the API call
   return (
     <div className="map">
-      {props.albums.results.slice(1).map((album, i) => /* Checking data.imgURL not equal to test avoids an error with displaying a bad painting */
-        <a target="_blank" rel="noopener noreferrer" href={album.collectionViewUrl}>
-          <div key={i} className="mappedAlbums">
-            <img src={album.artworkUrl100} alt={album.collectionName} />
-            <h2>{album.collectionName}</h2>
-            <h3>{album.artistName} ({album.releaseDate.substring(5,7)}/{album.releaseDate.substring(8,10)}/{album.releaseDate.substring(0,4)})</h3>
-          </div>
-        </a>
+      {props.albums.results.slice(1).map((album, i) => // the first result contains artist information, and must be excluded from this loop using slice
+          <a target="_blank" rel="noopener noreferrer" href={album.collectionViewUrl}> {/* This anchor tag redirects to the album page on Apple music */}
+            <div key={i} className="mappedAlbums">
+              <img src={album.artworkUrl100} alt={album.collectionName} /> {/* The album cover art, or the name of the album if the image fails to load */}
+              <h2>{album.collectionName}</h2> {/* The name of the album */}
+              <h3>{album.artistName} ({album.releaseDate.substring(5,7)}/{album.releaseDate.substring(8,10)}/{album.releaseDate.substring(0,4)})</h3> {/* The artist and date of album release */}
+            </div>
+          </a>
         )
-        }
-      </div>
+      }
+    </div>
   )
   } else {
-    return null;
+    return null; // Avoids an error where not all conditions returned a value
   }
 }
 
